0d58d88c2d77fc14754bfda7bf4f54eb05300e21
mininene <iranzof@gmail.com>
2020-03-15 00:32:10 +0100

    h

---
+++ EXAMEN_FINAL/Services/Log/ProdutionLog.cs
@@ -0,0 +1,29 @@
+﻿using EXAMEN_FINAL.InfraestructuraTransversal.ServicesException;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace EXAMEN_FINAL.Services.Log
+{
+    public class ProdutionLog : ILog
+    {
+        
+        public void WriteLog(string mensaje)
+        {
+
+            var date = DateTime.Now;
+
+            try
+            {
+                Console.WriteLine($"[{date}] {mensaje}");
+            }
+            catch (Exception ex)
+            {
+                throw new LogException("No se ha podido escribir en la consola", ex);
+            }
+        }
+
+    }
+}
